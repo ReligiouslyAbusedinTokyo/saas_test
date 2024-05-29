da9ff78182224b4ab83f3717637c70261c08dbad
原貴悌 <haratakayasu@raisondetre.tokyo>
2024-05-30 05:25:53 +0900

    add field to custom modal screen

--- src/components/forms/workflow-form.tsx
+++ src/components/forms/workflow-form.tsx
@@ -4,6 +4,28 @@ import { z } from 'zod'
 import { WorkflowFormSchema } from '@/lib/types'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useRouter } from 'next/navigation'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '../ui/card'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '../ui/form'
+import { Input } from '../ui/input'
+import { Button } from '../ui/button'
+import { Loader2 } from 'lucide-react'
+//import { toast } from 'sonner'
+//import { onCreateWorkflow } from '@/app/(main)/(pages)/workflows/_actions/workflow-connections'
+import { useModal } from '@/providers/modal-provider'
+
 
 type Props = {
     title?:string
@@ -24,8 +46,76 @@ const WorkflowForm = ({subTitle, title}: Props) => {
       const router = useRouter()
 
 
+     const handleSubmit=()=>{}
+
     return (
-    <div>WorkflowForm</div>
+    <Card className="w-full max-w-[650px] border-none">
+      {title && subTitle && (
+        <CardHeader>
+          <CardTitle>{title}</CardTitle>
+          <CardDescription>{subTitle}</CardDescription>
+        </CardHeader>
+        
+      )}
+      <CardContent>
+      <Form {...form}>
+        <form 
+         className="flex flex-col gap-4 text-left"
+        onSubmit={form.handleSubmit(handleSubmit)}>
+             <FormField
+              disabled={isLoading}
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="Name"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              disabled={isLoading}
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Input
+                     {...field}
+                      placeholder="Description"
+                     
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button
+              className="mt-4"
+              disabled={isLoading}
+              type="submit"
+            >
+              {isLoading ? (
+                <>
+                  <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Saving
+                </>
+              ) : (
+                'Save Settings'
+              )}
+            </Button>
+
+        </form>
+      </Form>
+
+      </CardContent>
+    </Card>
   )
 }
 
