86521e06c3b22d8b2b6401071a862387589ddefb
原貴悌 <haratakayasu@raisondetre.tokyo>
2024-05-30 02:20:07 +0900

    Connection page which allows users connect and wire up each SNS to be shared with other members

--- src/app/(main)/(pages)/connections/page.tsx
+++ src/app/(main)/(pages)/connections/page.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import {CONNECTIONS} from '@/lib/constant'
+import ConnectionCard from './_components/connection-card'
 
 type Props ={
     searchParams? :{ [key:string]:string | undefined}
@@ -14,7 +16,16 @@ const Connections =(props:Props) =>{
       <section className="flex flex-col gap-4 p-6 text-muted-foreground">
         Connect all your apps directly from here. You may need to connect
         these apps regularly to refresh verification
-        
+        {CONNECTIONS.map((connection)=>(
+          <ConnectionCard 
+          key={connection.title}
+          description={connection.description}
+          title={connection.title}
+          icon={connection.image}
+          type={connection.title}
+         // connected={}
+          />
+        ))}
       </section>
     </div>
   </div>)
